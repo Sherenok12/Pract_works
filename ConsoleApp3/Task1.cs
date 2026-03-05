4da4cb9ee9f37c33847e774771f3115239657606
Олександр <sherenok12@gmail.com>
2026-03-05 21:04:45 +0200

    практ1

---
+++ ConsoleApp3/Task1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Task1
+{
+    delegate double MathOperation(double a, double b);
+    static double Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    static double Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+    static double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    static double Divide(double a, double b)
+    {
+        if (b == 0)
+        {
+            Console.WriteLine("Ділити на 0 не можна");
+            return 0;
+        }
+        return a / b;
+    }
+
+   /* static void Main()
+    {
+        MathOperation operation;
+
+        double x = 4;
+        double y = 2;
+
+        operation = Add;
+        Console.WriteLine($"Додавання: {operation(x, y)}");
+
+        operation = Subtract;
+        Console.WriteLine($"Віднімання: {operation(x, y)}");
+
+        operation = Multiply;
+        Console.WriteLine($"Множення: {operation(x, y)}");
+
+        operation = Divide;
+        Console.WriteLine($"Ділення: {operation(x, y)}");
+    }*/
+}
---
+++ ConsoleApp3/task2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+class task2
+{
+    delegate void NotificationHandler(string message);
+
+    static void SendEmail(string message)
+    {
+        Console.WriteLine("Email sent: " + message);
+    }
+
+    static void SendSMS(string message)
+    {
+        Console.WriteLine("SMS sent: " + message);
+    }
+
+  /*  static void Main()
+    {
+        NotificationHandler notify = null;
+
+        notify += SendEmail;
+        notify += SendSMS;
+
+        notify("!!!!");
+    }*/
+}
