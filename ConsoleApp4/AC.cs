6ccaa22a78f7a7c6dbf27a65a266ed7c832aed46
Олександр <sherenok12@gmail.com>
2026-03-23 10:56:41 +0200

    практ 2(1)

---
+++ ConsoleApp4/AC.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class AC
+    {
+        public void Sub(TempSensor s)
+        {
+            s.tempChanged += Work;
+        }
+
+        void Work(int t)
+        {
+            if (t < 17)
+                Console.WriteLine("Увімкнено обігрів");
+            else if (t <= 25)
+                Console.WriteLine("Вимкнено");
+            else
+                Console.WriteLine("Увімкнено охолодження");
+        }
+    }
+}
---
+++ ConsoleApp4/TempSensor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class TempSensor
+    {
+        public event Action<int> tempChanged;
+
+        public void SetTemp(int t)
+        {
+            Console.WriteLine("\n Нова температура:: " + t);
+
+            if (tempChanged != null)
+            {
+                tempChanged(t);
+            }
+        }
+    }
+}
