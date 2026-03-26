9d77b41a20ca48e8f09598139b34a6cad1b272e9
Олександр <sherenok12@gmail.com>
2026-03-26 09:12:14 +0200

    кр1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp1
+{
+    public delegate void MessageEventHandler(string message);
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string logFile = "logPD22.txt";
+
+            File.WriteAllText(logFile, "");
+
+            var publisher = new MessagePublisher();
+            var log = new FileLogger(logFile);
+
+            log.Subscribe(publisher);
+
+            Console.WriteLine("Введіть текст 4 рази:");
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write($"Введіть рядок {i + 1}: ");
+                string input = Console.ReadLine();
+
+                publisher.Send(input);
+            }
+
+            Console.WriteLine($"Запис у файлі");
+        }
+    }
+}
